01b1bf321fdd8e86b5c844364d2ad07134bb8478
kumar3mihir <mihirkumarmallick@Mihirs-MacBook-Air.local>
2024-03-14 01:49:27 +0530

    Calender using react and tailwind css
    13 mar 2024

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Calendar from './pages/Calender'
+
+function App() {
+  return (
+    <>
+      <Calendar />
+    </>
+  );
+}
+
+export default App
